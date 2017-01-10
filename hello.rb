34554d9ff97bd98b65715a218e1f9f1db52a7fdb
olisa onwutalu <fairplace2@gmail.com>
2017-01-10 14:01:41 +0000

    Add files via upload

---
+++ hello.rb
@@ -0,0 +1,3 @@
+puts "Hello Olisa"
+open.
+exit
---
+++ mymethod.rb
@@ -0,0 +1,9 @@
+def my_math_method
+	number = 4 + 4
+	puts number
+end
+
+def my_math_method(num1, num2)
+    number = num1 + num2
+    puts number
+end
